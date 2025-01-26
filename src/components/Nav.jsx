ab98bba2b49d1deb8b568ca3b54f497b178706da
braguilera <braaguileraa@gmail.com>
2025-01-26 19:09:55 -0300

    botones del nav estilizados

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,80 +14,81 @@ const Nav = () => {
   return (
 
     <>
-    <main className='bg-white flex flex-col'>
-        <header>
-          Logo
-        </header>
+    <main className='bg-white flex flex-col justify-between py-10 w-52 text-2xl box-border'>
 
-        <section className='flex flex-col'>
-        <NavLink
-          to="dashboard"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-500 font-bold underline" // Clases para cuando está activo
-              : "text-gray-500 hover:text-gray-800" // Clases para cuando no está activo
-          }
-        >
-          Dashboard
-        </NavLink>
+        <section className='flex flex-col px-2 box-border'>
+          <header className='flex justify-center py-6'>
+            Logo
+          </header>
 
-        <NavLink
-          to="consultas"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-500 font-bold underline" // Clases para cuando está activo
-              : "text-gray-500 hover:text-gray-800" // Clases para cuando no está activo
-          }
-        >
-          Consultas
-        </NavLink>
+          <NavLink
+            to="dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? "text-light-accent bg-light-muted px-2 py-1 my-2 rounded-lg font-semibold transition-all duration-200 select-none" 
+                : "text-blue-300 px-2 font-semibold py-1 my-2 hover:text-light-accent hover:transition-all hover:duration-200"
+            }
+          >
+            Dashboard
+          </NavLink>
 
-        <NavLink
-          to="pacientes"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-500 font-bold underline" // Clases para cuando está activo
-              : "text-gray-500 hover:text-gray-800" // Clases para cuando no está activo
-          }
-        >
-          Pacientes
-        </NavLink>
+          <NavLink
+            to="consultas"
+            className={({ isActive }) =>
+              isActive
+                ? "text-light-accent bg-light-muted px-2 py-1 my-2 rounded-lg font-semibold transition-all duration-200 " 
+                : "text-blue-300 px-2 font-semibold py-1 my-2 hover:text-light-accent hover:transition-all hover:duration-200"
+            }
+          >
+            Consultas
+          </NavLink>
 
-        <NavLink
-          to="medicos"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-500 font-bold underline" // Clases para cuando está activo
-              : "text-gray-500 hover:text-gray-800" // Clases para cuando no está activo
-          }
-        >
-          Medicos
-        </NavLink>
+          <NavLink
+            to="pacientes"
+            className={({ isActive }) =>
+              isActive
+                ? "text-light-accent bg-light-muted px-2 py-1 my-2 rounded-lg font-semibold transition-all duration-200 " 
+                : "text-blue-300 px-2 font-semibold py-1 my-2 hover:text-light-accent hover:transition-all hover:duration-200"
+            }
+          >
+            Pacientes
+          </NavLink>
+
+          <NavLink
+            to="medicos"
+            className={({ isActive }) =>
+              isActive
+                ? "text-light-accent bg-light-muted px-2 py-1 my-2 rounded-lg font-semibold transition-all duration-200"
+                : "text-blue-300 px-2 font-semibold py-1 my-2 hover:text-light-accent hover:transition-all hover:duration-200"
+            }
+          >
+            Medicos
+          </NavLink>
 
 
-        <NavLink
-          to="servicios"
-          className={({ isActive }) =>
-            isActive
-              ? "text-blue-500 font-bold underline" // Clases para cuando está activo
-              : "text-gray-500 hover:text-gray-800" // Clases para cuando no está activo
-          }
-        >
-          Servicios
-        </NavLink>
+          <NavLink
+            to="servicios"
+            className={({ isActive }) =>
+              isActive
+                ? "text-light-accent bg-light-muted px-2 py-1 my-2 rounded-lg font-semibold transition-all duration-200 "
+                : "text-blue-300 px-2 font-semibold py-1 my-2 hover:text-light-accent hover:transition-all hover:duration-200"
+            }
+          >
+            Servicios
+          </NavLink>
 
         </section>
 
-
-    </main>
-
     <footer>
 
-      <button onClick={logout}>
+      <button onClick={logout} className='text-gray-400 hover:text-gray-600 px-2 text-xl'>
         Cerrar sesión
       </button>
 
     </footer>
+
+
+    </main>
 
     </>
   )
